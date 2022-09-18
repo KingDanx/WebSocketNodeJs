33caacc29447f00446a51c8e474c990b16dc01fa
Dan <dannyprud@yahoo.com>
2022-09-18 08:10:21 -0500

    Add third client for testing how elements are generated and sorted.

---
+++ clients/js/index3.js
@@ -0,0 +1,44 @@
+import TOKEN from "../config/config.js";
+// Create WebSocket connection.
+
+const connect = () => {
+  const socket = new WebSocket("ws://localhost:3000", [TOKEN, "DogClient"]);
+  const switcherInterval = setInterval(sendSwticher, 100);
+  // Connection opened
+  socket.addEventListener("open", function (event) {
+    console.log("Connected to WS Server");
+    console.log(socket.readyState);
+  });
+
+  // Listen for messages
+  socket.addEventListener("message", function (event) {
+    console.log(event.data);
+  });
+
+  socket.addEventListener("close", (event) => {
+    console.log("Disconnected from server");
+    const reconnectInterval = setInterval(() => {
+      socket.close();
+      clearInterval(switcherInterval);
+      clearInterval(reconnectInterval);
+      connect();
+    }, 1000);
+  });
+
+  window.sendMessage = function () {
+    socket.send("Hello From Client3!");
+  };
+
+  function sendSwticher() {
+    socket.send(switcher);
+  }
+};
+
+connect();
+
+let switcher = false;
+
+window.changeSwitcher = function () {
+  switcher == false ? (switcher = true) : (switcher = false);
+  document.getElementById("switch-value").innerHTML = switcher;
+};
